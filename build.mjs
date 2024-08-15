630ec3aaa639f5406e7070ca0d536a39f6a39667
Oliver Rademaker <oliver.rademaker@hiscale.com>
2024-08-15 14:33:10 +0200

    update build script

--- build.mjs
+++ build.mjs
@@ -3,7 +3,7 @@ import * as esbuild from 'esbuild'
 
 await esbuild.build({
     entryPoints: ['src/index.js'],
-    outfile: 'dist/ddns-fw-3.js',
+    outfile: 'dist/ddns-broker.js',
     platform: 'node',
     bundle: true,
     sourcemap: true
